a457fcbc5e6131c5ed5810689fc894e6472964f7
hllj <vanhop3499@gmail.com>
2024-06-30 04:07:57 +0000

    update CLI

--- llava/serve/cli.py
+++ llava/serve/cli.py
@@ -29,6 +29,11 @@ def main(args):
     disable_torch_init()
 
     model_name = get_model_name_from_path(args.model_path)
+    
+    # FIXME: Vistral-V is need to be fixed, add prefix 'llava-'
+    if 'vistral-v' in model_name.lower():
+        model_name = 'llava-' + model_name
+    
     tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, args.model_base, model_name, args.load_8bit, args.load_4bit, device=args.device)
 
     if "llama-2" in model_name.lower():
